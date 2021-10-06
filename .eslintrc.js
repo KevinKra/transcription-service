b65ac903ab1fcc4b89dcceb660195340fc11511b
KevinKra <kratok08@gmail.com>
2021-10-06 15:54:24 -0400

    configure linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: ["react", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:testing-library/react",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+  },
+};
