4cc4c167546d02e734e1511cc51cf79426753df7
KevinKra <kratok08@gmail.com>
2021-10-08 21:26:23 -0400

    add temp testing steps

--- src/components/_forms/SelectMedia/SelectMedia.test.tsx
+++ src/components/_forms/SelectMedia/SelectMedia.test.tsx
@@ -41,16 +41,44 @@ describe("SelectMedia", () => {
       ).toBeDisabled();
     });
   });
+
   describe("when a user inputs a url for a youtube video", () => {
-    describe("if the url is valid", () => {
-      test.todo("the VideoPlayer activates with the provided content");
-      test.todo("the sourceLanguage input is no longer disabled");
-      test.todo("the targetLanguage input is no longer disabled");
-      test.todo("the submit button is still disabled");
+    beforeEach(() => {
+      render(<SelectMedia />);
     });
+
+    // todo figure out how to trigger an update to redux:
+    // todo video found -> update author and media
+
+    describe("if the url is valid", () => {
+      test.skip("the VideoPlayer activates with the provided content", () => {
+        expect(
+          screen.queryByTestId(/video-player-enabled/i)
+        ).toBeInTheDocument();
+      });
+
+      test.skip("the sourceLanguage input is no longer disabled", () => {
+        expect(
+          screen.getByTestId(/input-select-source-language/i)
+        ).toBeEnabled();
+      });
+
+      test.skip("the targetLanguage input is no longer disabled", () => {
+        expect(
+          screen.getByTestId(/input-select-target-language/i)
+        ).toBeEnabled();
+      });
+
+      test.skip("the submit button is still disabled", () => {
+        expect(
+          screen.getByRole("button", { name: /build lesson/i })
+        ).toBeDisabled();
+      });
+    });
+
     describe("if the url is invalid", () => {
       test.todo("the VideoPlayer does not activate");
-      test.todo("a failure snackbar appears");
+      // test.todo("a failure snackbar appears");
       test.todo("the sourceLanguage remains disabled");
       test.todo("the targetLanguage remains disabled");
       test.todo("the submit button remains disabled");
