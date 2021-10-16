998e174c0c2cdb5a014f69759f2a18612a1583c7
KevinKra <kratok08@gmail.com>
2021-10-16 16:00:19 -0400

    add comments to SnackBar test

--- src/components/_atoms/SnackBar/StyledSnackBar.test.tsx
+++ src/components/_atoms/SnackBar/StyledSnackBar.test.tsx
@@ -1,20 +1,34 @@
-import { render } from "@testing-library/react";
+import { render } from "../../../testing-utils/test-utils";
 import StyledSnackBar from "./StyledSnackBar";
-// import user from "@testing-library/user-event";
 
-jest.mock("react-redux", () => {
-  return {
-    ...jest.requireActual("react-redux"),
-    useSelector: jest.fn().mockImplementation(() => ({})),
-    useDispatch: () => jest.fn(),
-  };
-});
+// jest.mock("react-redux", () => {
+//   return {
+//     ...jest.requireActual("react-redux"),
+//     store: {
+//       alert: {
+//         type: "warning",
+//         message: "No media has been provided to the video player.",
+//         display: "client-only",
+//         key: 1634411717212,
+//       },
+//     },
+//   };
+// });
 
 describe("StyledSnackBar", () => {
   beforeEach(() => {
     render(<StyledSnackBar />);
   });
-  test.todo("if the redux alertState object has a key, setOpen to true");
 
   test.todo("if close button is clicked, setOpen is set to false");
+  // eslint-disable-next-line jest/no-commented-out-tests
+  // test("if close button is clicked, setOpen is set to false", () => {
+  // expect(
+  //   screen.queryByText("No media has been provided to the video player.")
+  // ).toBeInTheDocument();
+
+  // click escape button
+
+  // assert toast is no longer in the document
+  // });
 });
