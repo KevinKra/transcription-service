2f0c28c25c234564d304fb48d1d6b01c4948ac9c
KevinKra <kratok08@gmail.com>
2021-10-17 12:54:37 -0400

    add author to redux, setup implementation to include on fetch

---
+++ src/redux/slices/authorSlice/authorSlice.ts
@@ -0,0 +1,46 @@
+// ? param-reassign is fine, RTK uses immer (https://redux-toolkit.js.org/tutorials/quick-start)
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+export interface IAuthor {
+  id: string;
+  name: string;
+  youtube: string;
+  patreon: string;
+  primaryLanguage: string;
+  primaryTopic: string;
+  channelURL: string;
+  userURL: string;
+  thumbnails: { height: number; width: number }[];
+}
+
+const initialState: IAuthor = {
+  id: "",
+  name: "",
+  youtube: "",
+  patreon: "",
+  primaryLanguage: "",
+  primaryTopic: "",
+  channelURL: "",
+  userURL: "",
+  thumbnails: [],
+};
+
+export const authorSlice = createSlice({
+  name: "author",
+  initialState,
+  reducers: {
+    setAuthor: (_, action: PayloadAction<IAuthor>) => action.payload,
+  },
+});
+
+export const selectAuthor = createSelector(
+  (state: RootState) => state.author,
+  (author) => author
+);
+
+export const { setAuthor } = authorSlice.actions;
+
+export default authorSlice.reducer;
