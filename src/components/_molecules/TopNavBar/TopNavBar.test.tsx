f32f7b6603f23efa44f572143d758b2de58566a3
KevinKra <kratok08@gmail.com>
2021-10-17 21:58:02 -0400

    Write initial tests for TopNavBar

--- src/components/_molecules/TopNavBar/TopNavBar.test.tsx
+++ src/components/_molecules/TopNavBar/TopNavBar.test.tsx
@@ -1,17 +1,37 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "../../../testing-utils/test-utils";
 import TopNavBar from "./TopNavBar";
 
 describe("TopNavBar", () => {
   describe("when the component mounts", () => {
-    beforeEach(() => {
-      render(
-        <TopNavBar
-          switchToDarkMode={jest.fn()}
-          switchToLightMode={jest.fn()}
-          darkModeActive={false}
-        />
-      );
+    describe("and no user is logged in", () => {
+      beforeEach(() => {
+        render(
+          <TopNavBar
+            switchToDarkMode={jest.fn()}
+            switchToLightMode={jest.fn()}
+            darkModeActive={false}
+          />
+        );
+      });
+      test("main title is present", () => {
+        expect(screen.queryByText(/parakeet/i)).toBeInTheDocument();
+      });
+
+      test("create Lesson link is present", () => {
+        expect(screen.queryByText(/create lesson/i)).toBeInTheDocument();
+      });
+
+      test("the 'sign in' button is present", () => {
+        expect(
+          screen.queryByRole("button", { name: /sign in/i })
+        ).toBeInTheDocument();
+      });
+
+      test("the 'sign up' button is present", () => {
+        expect(
+          screen.queryByRole("button", { name: /sign up/i })
+        ).toBeInTheDocument();
+      });
     });
-    test.todo("write tests for this component");
   });
 });
